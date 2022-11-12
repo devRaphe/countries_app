41ed681d998050627f5fe699876e447cb7f2c829
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2022-11-12 16:49:31 +0100

    initialized service locator and screen util

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -7,7 +8,8 @@ import 'app/app.router.dart';
 import 'core/theme/countries_theme_manager.dart';
 import 'services/theme_service/theme_service.dart';
 
-void main() {
+void main() async {
+  await setupLocator();
   runApp(const CountriesApp());
 }
 
@@ -21,13 +23,20 @@ class CountriesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder.reactive(
       viewModelBuilder: CountryAppViewModel.new,
-      builder: (context, model, child) => MaterialApp(
-        title: 'Countries App',
-        theme: CountriesThemeManager.lightTheme,
-        darkTheme: CountriesThemeManager.darkTheme,
-        themeMode: model.themeMode,
-        onGenerateRoute: StackedRouter().onGenerateRoute,
-        navigatorKey: StackedService.navigatorKey,
+      builder: (context, model, child) => ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: model.themeMode,
+            darkTheme: CountriesThemeManager.darkTheme,
+            theme: CountriesThemeManager.lightTheme,
+            navigatorKey: StackedService.navigatorKey,
+            onGenerateRoute: StackedRouter().onGenerateRoute,
+          );
+        },
       ),
     );
   }
