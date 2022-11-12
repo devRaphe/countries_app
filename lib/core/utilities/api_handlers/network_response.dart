54372b9b15b8aa963a118c57501539e27aa60f72
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2022-11-12 04:24:42 +0100

    Create network_response.dart

---
+++ lib/core/utilities/api_handlers/network_response.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+/// This class services as a value object for the http network response
+class NetworkResponse extends Equatable {
+  /// Default constructor
+  const NetworkResponse({
+    required this.data,
+    required this.statusCode,
+  });
+
+  /// Contains the data response from the network or API.
+  final dynamic data;
+
+  /// Contains the status code of the network response.
+  final int? statusCode;
+
+  @override
+  bool? get stringify => true;
+
+  @override
+  List<Object?> get props => [data, statusCode];
+}
