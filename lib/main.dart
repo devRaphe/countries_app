57f017f4fcfcb87aadf093f7f5f9fb81671fdad9
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2022-11-12 16:40:06 +0100

    configure app to use custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+import 'app/app.locator.dart';
 import 'app/app.router.dart';
+import 'core/theme/countries_theme_manager.dart';
+import 'services/theme_service/theme_service.dart';
 
 void main() {
   runApp(const CountriesApp());
@@ -15,13 +19,28 @@ class CountriesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: CountryAppViewModel.new,
+      builder: (context, model, child) => MaterialApp(
+        title: 'Countries App',
+        theme: CountriesThemeManager.lightTheme,
+        darkTheme: CountriesThemeManager.darkTheme,
+        themeMode: model.themeMode,
+        onGenerateRoute: StackedRouter().onGenerateRoute,
+        navigatorKey: StackedService.navigatorKey,
       ),
-      onGenerateRoute: StackedRouter().onGenerateRoute,
-      navigatorKey: StackedService.navigatorKey,
     );
   }
+}
+
+/// Viewmodel for the [CountriesApp] widget.
+/// The viewmmodel only set dynamic data to the view, such as [ThemeMode]
+class CountryAppViewModel extends ReactiveViewModel {
+  final _themeService = locator<ThemeService>();
+
+  /// This getter returns the current theme mode.
+  ThemeMode get themeMode => _themeService.themeMode;
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [_themeService];
 }
