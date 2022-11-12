27aab3a8ea33ff0097e9e6ce4d4dd79e99ff024c
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2022-11-12 04:35:09 +0100

    registerd network service in service locator

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+import '../services/network_service/dio_network_service.dart';
+import '../services/network_service/i_network_service.dart';
 import '../ui/countries/countries_view.dart';
 import '../ui/countries_detail/countries_detail_view.dart';
 
@@ -11,7 +13,11 @@ import '../ui/countries_detail/countries_detail_view.dart';
   ],
   dependencies: [
     LazySingleton<NavigationService>(classType: NavigationService),
-    LazySingleton<BottomSheetService>(classType: BottomSheetService)
+    LazySingleton<BottomSheetService>(classType: BottomSheetService),
+    LazySingleton<INetworkService>(
+      classType: DioNetworkService,
+      asType: INetworkService,
+    ),
   ],
   logger: StackedLogger(),
 )
