5adba6d35bebe0c04b60a88e7a8c65f66348f896
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2022-11-12 09:06:46 +0100

    moved api_constants into a class for better usage.

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,15 +1,19 @@
-// Base Related
+/// This class holds all the endpoints to be used in this app.
+class ApiConstants {
+  // Base Related
 
-///
-const String _scheme = 'https';
+  ///
+  static const String _scheme = 'https';
 
-///
-const String _host = 'restcountries.com';
+  ///
+  static const String _host = 'restcountries.com';
 
-///This is the base uri for the countries app
-Uri baseUri = Uri(scheme: _scheme, host: _host, path: '/');
+  ///This is the base uri for the countries app
+  Uri baseUri = Uri(scheme: _scheme, host: _host, path: '/');
 
 // Countries Payments Endpoints
 
-/// This is the endpoint for getting all countries
-Uri allCountriesEndpoint = Uri(scheme: _scheme, host: _host, path: '/v3.1/all');
+  /// This is the endpoint for getting all countries
+  Uri getAllCountriesEndpoint =
+      Uri(scheme: _scheme, host: _host, path: '/v3.1/all');
+}
