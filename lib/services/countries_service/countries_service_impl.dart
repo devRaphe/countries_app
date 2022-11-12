02a7bde2399ccad2c7ecb034e1304b91cb667834
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2022-11-12 14:08:33 +0100

    added logs in countries  service

--- lib/services/countries_service/countries_service_impl.dart
+++ lib/services/countries_service/countries_service_impl.dart
@@ -1,4 +1,5 @@
 import '../../app/app.locator.dart';
+import '../../app/app.logger.dart';
 import '../../core/constants/api_constants.dart';
 import '../../models/country.dart';
 import '../network_service/i_network_service.dart';
@@ -6,19 +7,26 @@ import 'i_countries_service.dart';
 
 /// This is the implementation of the [ICountriesService] interface.
 class CountriesServiceImpl extends ICountriesService {
+  final _log = getLogger('CountriesServiceImpl');
   final INetworkService _networkService = locator<INetworkService>();
 
   /// Provides a list of countries.
 
   @override
   Future<List<Country>> getCountries() async {
+    _log.v('Fetching countries...');
     final response = await _networkService
         .get<List<Map<String, dynamic>>>(ApiConstants.getAllCountriesEndpoint);
+    _log
+      ..v('Countries fetched successfully.\n ${response.data}')
+      ..v('Parsing countries...');
+
     final countries = List<Country>.from(
       response.data.map(
         Country.fromJson,
       ),
     );
+    _log.v('Countries parsed successfully.');
     return countries;
   }
 
